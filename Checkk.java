9ccb4403eaed01c10293ed9f282231c52e38e8f9
Sowndharya13197 <sowndharyagopal13@gmail.com>
2017-06-05 07:41:28 -0700

    Create Checkk.java

---
+++ Checkk.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Checkk {
+    public static void main(String args[])
+    {
+        System.out.println("enter no");
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        if(n==0)
+        {
+            System.out.println("zero");
+        }
+     if(n>0)
+        {
+            System.out.println("positive");
+        }
+     if(n<0)
+     {System.out.println("negative");
+        
+}}
+}
